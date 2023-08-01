9118042551c0926e4f55f13b0948224e55caf746
Rolando Peña <angel.rolandop@gmail.com>
2023-07-31 20:09:03 -0500

    Update data.tf

--- tf/data.tf
+++ tf/data.tf
@@ -1,7 +1,9 @@
 data "aws_eks_cluster" "default" {
-  name = "${var.app_name}_cluster"
+   name = "${var.app_name}-cluster"
+   depends_on = [module.eks.cluster_name]
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = "${var.app_name}_cluster"
+   name = "${var.app_name}-cluster"
+   depends_on = [module.eks.cluster_name]
 }
